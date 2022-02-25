63a71cb2ca5538e2ff4e70f75af54f86f4a72059
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2022-02-26 03:00:24 +0900

    Update: Improving performance

--- src/libs/twitter.ts
+++ src/libs/twitter.ts
@@ -8,27 +8,48 @@ declare global {
   }
 }
 
+const isFulfilled = <T,>(input: PromiseSettledResult<T>): input is PromiseFulfilledResult<T> =>
+  input.status === 'fulfilled'
+
 // 非同期関数を定義
-const loadWidgetAsync = () =>
-  new Promise<void>((resolve, reject) => {
-    if (!window.twttr) {
-      const msg = 'Failure to load window.twttr, aborting load'
-      console.error(msg)
-      reject(msg)
-    } else {
-      try {
-        // <script> タグ内で定義した window.twttr.widgets をロードする
-        window.twttr.widgets.load(document.getElementsByTagName('article'))
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
-    }
-  })
+const processTweetsAsync = async (blockquotes: HTMLCollectionOf<Element>) => {
+
+  const loadEmbedTweet = window.twttr.widgets.load
+
+  // Promise<void> を要素とする配列を並列して解決させる
+  const data = await Promise.allSettled(
+    // Promise<void> を要素とする配列を作る
+    Array.from(blockquotes).map(
+      (quote) =>
+        new Promise<void>((resolve, reject) => {
+          // console.log(quote)  // dom が帰る
+          try {
+            if (!quote.hasAttribute('class')) reject("quote doesn\'t have class")
+
+            const className = quote.getAttribute('class').split(/\s/)
+
+            if (!(className.includes('twitter-tweet'))) {
+              reject()
+            } else {
+              // <blockquote> かつ少なくとも 'class' 属性は持っており，
+              // そこに 'twitter-tweet' という値が含まれる Element に対して，
+              // <script> タグ内で定義した window.twttr.widgets をロードする
+              loadEmbedTweet(quote)
+              resolve()
+            }
+          } catch (e) {
+            reject(e)
+          }
+        })
+    )
+  )
+
+  return data.filter(isFulfilled).map((r) => r.value)
+}
 
 // ページ遷移してから少しの間ディレイを入れる
 // ページ内の他のコンテンツの読み込みを優先させるため
-const delay = 500
+const delay = 100
 
 export const useTweetEmbed = () => {
   const isBrowser = useIsBrowser()
@@ -36,7 +57,8 @@ export const useTweetEmbed = () => {
 
   // Debounced function
   useEffect(() => {
-    const fn = async () => await loadWidgetAsync()
+    const blockquote = document.getElementsByTagName('blockquote');
+    const fn = async () => await processTweetsAsync(blockquote)
     const timer = setTimeout(() => {
       if (!isBrowser) return
       void fn() // ここまで到達して初めて非同期関数を実行
